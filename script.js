610ad3f6d9b8df49e067a0b730d49e211a9e463e
eds0ng <edwardsong165@gmail.com>
2015-11-23 22:37:02 -0800

    minify and uglify

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var myapp = angular.module('myapp', ["ui.router"]);
 myapp.service('planetService', function (){
   var planets = [];
   this.populatePlanets = function (results){
-    planets = results.map(planet => {
-      planet.residents = planet.residents.map(resident => {
+    planets = results.map(function(planet) {
+      planet.residents = planet.residents.map(function(resident) {
         var resident = { url: resident };
         resident.id = resident.url.match(/\d+/)[0];
         return resident;
@@ -35,33 +35,35 @@ myapp.service('characterService', function (){
   }
 })
 
-myapp.controller("ResidentCtrl", function($scope, $http, $stateParams, characterService) {
+myapp.controller("ResidentCtrl", ['$scope', '$http', '$stateParams', 'characterService', function($scope, $http, $stateParams, characterService) {
   $scope.nextChar = Number($stateParams.id)+1;
   if(characterService.getCharacter($stateParams.id)){
     $scope.character = characterService.getCharacter($stateParams.id);
   }else{
-    $http.get("http://swapi.co/api/people/" + $stateParams.id + "/?format=json").then(resp => {
+    $http.get("http://swapi.co/api/people/" + $stateParams.id + "/?format=json").then(function(resp) {
         characterService.saveCharacter(resp.data, $stateParams.id);
         $scope.character = characterService.getCharacter($stateParams.id);
     });
   }
 
-})
+}])
 
-myapp.controller("PlanetCtrl", function($scope, $http, planetService, characterService) {
+myapp.controller("PlanetCtrl", ['$scope','$http', 'planetService', 'characterService',function($scope, $http, planetService, characterService) {
   if (planetService.getPlanets().length===0){
-    $http.get("http://swapi.co/api/planets/?format=json").then(resp => {
+    $http.get("http://swapi.co/api/planets/?format=json").then(function(resp) {
       planetService.populatePlanets(resp.data.results);
       $scope.planets = planetService.getPlanets();
 
-    }).catch(error => console.error(error.status));
+    }).catch(function(error){
+      console.error(error.status);
+    }); 
   }else{
     $scope.characters = characterService.getAll();
     $scope.planets = planetService.getPlanets();
   }
-});
+}]);
 
-myapp.config(function($stateProvider, $urlRouterProvider){
+myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider){
 
   $urlRouterProvider.otherwise("/planets")
 
@@ -76,4 +78,4 @@ myapp.config(function($stateProvider, $urlRouterProvider){
         templateUrl: "resident.html",
         controller: "ResidentCtrl"
     })
-})
+}])
